4ababb1fae2680905828334d48cf70b7e6903c75
Alex Rouse <alex.rouse@raizlabs.com>
2015-12-07 12:42:43 -0500

    fixing issue with not scrolling to the bottom

--- QBImagePicker/QBAssetsViewController.m
+++ QBImagePicker/QBAssetsViewController.m
@@ -101,8 +101,10 @@ static CGSize CGSizeScale(CGSize size, CGFloat scale) {
     
     [self updateDoneButtonState];
     [self updateSelectionInfo];
-    [self.collectionView reloadData];
-    
+
+    [self.collectionView setNeedsLayout];
+    [self.view layoutIfNeeded];
+
     // Scroll to bottom
     if (self.fetchResult.count > 0 && self.isMovingToParentViewController && !self.disableScrollToBottom) {
         NSIndexPath *indexPath = [NSIndexPath indexPathForItem:(self.fetchResult.count - 1) inSection:0];
